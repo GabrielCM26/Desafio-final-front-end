ab10ec58ed674e3b9c75e02306119d0112f617a9
Gabriel Moreira <gabrielmoreira7301@gmail.com>
2025-10-01 12:29:02 +0100

    pagina perfil mudadada para top100arti

--- src/pages/top100Artists.js
+++ src/pages/top100Artists.js
@@ -1,19 +1,19 @@
 import Link from 'next/link';
-import { obterTopMusicas } from '../utils/dataProcessing';
+import { obterTopArtistas } from '../utils/dataProcessing';
 
 export default function Top100() {
-  const topMusicas = obterTopMusicas(100);
+  const topArtistas = obterTopArtistas(100);
 
   return (
     <div className="max-w-[600px] mx-auto my-10 p-6 bg-[#f9f9f9] rounded-xl shadow-md">
       <h1 className="text-center text-4xl font-bold text-[#2a3a5e] mb-8 tracking-wide shadow-[0_2px_8px_rgba(42,58,94,0.15)]">
-        🎵 Top 100 Músicas Mais Ouvidas 🎵
+        🎵 Top 100 Artistas Mais Ouvidos 🎵
       </h1>
       <ol className="pl-6 text-lg">
-        {topMusicas.map((musica, idx) => (
+        {topArtistas.map((item, idx) => (
           <li key={idx}>
-            <span className="text-[#222] font-bold">{musica.nome}</span>
-            <span className="text-[#888] ml-2"> — {musica.contagem} vezes</span>
+            <span className="text-[#222] font-bold">{item.artista}</span>
+            <span className="text-[#888] ml-2"> — {item.plays} vezes</span>
           </li>
         ))}
       </ol>
