944b68b1e2a7e9380b43f3dbf64d3b3988969dba
Felix <felix.erwin02@gmail.com>
2025-10-01 12:39:58 +0100

    top100artists edit

--- src/pages/top100Artists.js
+++ src/pages/top100Artists.js
@@ -1,21 +1,35 @@
-import Link from 'next/link';
-import { obterTopMusicas } from '../utils/dataProcessing';
+// import { encontrarArtistaMaisOuvido } from '../utils/dataProcessing';
 
 export default function Top100() {
-  const topMusicas = obterTopMusicas(100);
+  // const topArtistas = encontrarArtistaMaisOuvido(100);
 
   return (
-    <div className="max-w-[600px] mx-auto my-10 p-6 bg-[#f9f9f9] rounded-xl shadow-md">
-      <h1 className="text-center text-4xl font-bold text-[#2a3a5e] mb-8 tracking-wide shadow-[0_2px_8px_rgba(42,58,94,0.15)]">
-        🎵 Top 100 Músicas Mais Ouvidas 🎵
-      </h1>
-      <ol className="pl-6 text-lg">
-        {topMusicas.map((musica, idx) => (
-          <li key={idx}>
-            <span className="text-[#222] font-bold">{musica.nome}</span>
-            <span className="text-[#888] ml-2"> — {musica.contagem} vezes</span>
+    <div className="max-w-[440px] mx-auto my-10 p-6 rounded-xl shadow-md">
+      <div>
+        <h1 className="text-left text-2xl font-bold mb-8">
+          Your Top 100 Artists
+        </h1>
+        <div className="flex flex-col bg-[#121212] rounded-[10px] mb-3 items-center">
+          <div className="flex flex-row space-between text-center">
+            <button className="bg-white/50 p-2 px-4">4 weeks</button>
+            <button className="bg-white/50 p-2 px-4">6 months</button>
+            <button className="bg-white/50 p-2 px-4">1 year</button>
+            <button className="bg-white/50 p-2 px-4">All time</button>
+          </div>
+          <div className="flex size-93 bg-[url(/img/artist_icon_top_100.jpg)] rounded-[10px] bg-cover">
+            <p>#1 Kendrick Lamar</p>
+          </div>
+        </div>
+      </div>
+      <ol className="text-lg">
+        {/* Importar código abaixo */}
+        {/* {topArtistas.map((perfil, idx) => ( */}
+        {/* Põe "key={index}" dentro de <li>*/}
+          <li className="bg-linear-to-r from-[#212121] from-50% to-[#121212] to-100% p-3 rounded-[10px] flex flex-row gap-3">
+            <span className="font-bold pl-3 pt-1">#2</span>
+            <img className="ml-2 rounded-full" src="../img/artist_icon_top_100_s.png" />
+            <span className="pt-1">Placeholder</span>
           </li>
-        ))}
       </ol>
     </div>
   );
