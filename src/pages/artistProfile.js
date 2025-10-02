77d355ee0746aa8f74775da1049443df05723de0
joaolourenco22 <joao_22lourenco@hotmail.com>
2025-10-02 17:56:31 +0100

    added images a public and upgraded artistsProfile

--- src/pages/artistProfile.js
+++ src/pages/artistProfile.js
@@ -6,24 +6,38 @@ export default function PerfilArtista() {
           <a href="#">
             <img className="rounded-lg w-screen h-auto object-cover" src="./img/kendrick.png" alt="Kendrick Lamar" />
           </a>
-          <figcaption className="absolute px-4 text-lg text-white bottom-6">
+          <figcaption className="absolute px-4 text-lg text-white bottom-3">
             <p className="text-xs text-amber-300">☀️ artist</p>
-            <p>KENDRICK LAMAR</p>
+            <p className="text-4xl">KENDRICK LAMAR</p>
             <p className="text-xs">TOP #6</p>
           </figcaption>
         </figure>
       </div>
 
-      <div className="bg-linear-to-r from-[#212121] from-50% to-[#121212] to-100% p-3 rounded-[10px]">div comprida</div>
+      <div className="p-8 space-y-6 justify-">
+        <div className="bg-linear-to-r from-[#212121] from-50% to-[#121212] to-100% rounded-[10px] p-4">
+          <div className="flex flex-row gap-4 items-center">
+            <img src="./img/botao_play_pag_artist.png" alt="Imagem botão play" className="w-12 h-12 mb-2" />
+            <div className="flex flex-col items-center">
+              <p className="text-4xl">12,4K</p>
+              <p className="text-xs">nº de plays</p>
+            </div>
+          </div>
 
-    <div className="flex flex-row gap-4 mt-4">
-      <div className="bg-linear-to-r from-[#212121] from-50% to-[#121212] to-100% p-3 rounded-[10px]">Hello, World!</div>
-      <div className="bg-linear-to-r from-[#212121] from-50% to-[#121212] to-100% p-3 rounded-[10px]">Hello, World!</div>
-    </div>
+        </div>
 
-      <div className="bg-green-700 text-white rounded-lg fixed bottom-14 left-0 right-0 p-2 mx-4 flex items-center justify-center">
-        <p className="text-center text-2xl font-black  ">TOP 20</p>
+        <div className="flex flex-row gap-4 mt-4">
+          <div className="bg-linear-to-r from-[#212121] from-50% to-[#121212] to-100% p-3 rounded-[10px]">nº de musicas played</div>
+          <div className="bg-linear-to-r from-[#212121] from-50% to-[#121212] to-100% p-3 rounded-[10px]">% das plays dentro do total</div>
+        </div>
+
+        <div className="bg-linear-to-r from-[#212121] from-50% to-[#121212] to-100% p-3 rounded-[10px]">nº de plays</div>
+
+        <div className="bg-green-700 text-white rounded-lg fixed bottom-14 left-0 right-0 p-2 mx-4 flex items-center justify-center">
+          <p className="text-center text-2xl font-black">TOP 20</p>
+        </div>
       </div>
+
     </div>
 
   )
