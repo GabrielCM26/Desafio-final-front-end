3fff2867daac0d269635dcccb23d530815c98378
gcunha07 <goncalon2107@gmail.com>
2025-10-03 13:04:49 +0100

    perfil na home page check

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,30 @@ export default function Home() {
     <div className="">
       <div className="">
         <div className="space-y-6">
-          <div className="bg-gradient-to-r from-green-600 to-blue-600 text-white rounded-lg shadow-lg p-8 text-center">
-            <h1 className="text-4xl md:text-6xl font-bold mb-4">Bem-vindo ao Spotidados! 🎵</h1>
-            <p className="text-xl md:text-2xl mb-6 opacity-90">Descubra insights incríveis sobre seus hábitos musicais</p>
+          <div className="bg-gradient-to-b from-green-500 to-black-600 text-white rounded-lg shadow-lg p-8 text-center">
+            <div class="profile">
+              <div class="profile">
+
+                <div class="profile-img">
+
+                  <svg height="40" width="40" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 0h24v24H0z" fill="none"></path>
+                    <path fill="white" d="M4.828 21l-.02.02-.021-.02H2.992A.993.993 0 0 1 2 20.007V3.993A1 1 0 0 1 2.992 3h18.016c.548 0 .992.445.992.993v16.014a1 1 0 0 1-.992.993H4.828zM20 15V5H4v14L14 9l6 6zm0 2.828l-6-6L6.828 19H20v-1.172zM8 11a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"></path>
+                  </svg>
+
+
+                  <button class="edit-btn" title="Editar perfil">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" fill="white" viewBox="0 0 24 24">
+                      <path d="M3 17.25V21h3.75l11.06-11.06-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 
+                    0-1.41l-2.34-2.34a1 1 0 0 0-1.41 0l-1.83 1.83 
+                    3.75 3.75 1.83-1.83z"/>
+                    </svg>
+                  </button>
+                </div>
+
+              </div>
+              <span class="profile-heading">João Lourenço</span>
+            </div>
           </div>
           {/*aquiiiiiiiii joao */}
           <div className="cards-container">
@@ -74,10 +95,11 @@ export default function Home() {
 
 
           </div>
+
         </div>
+
       </div>
 
     </div>
-
   )
 }
