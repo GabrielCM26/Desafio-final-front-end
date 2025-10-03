b06ba906743195ea876e997790209770c746d5d0
Felix <felix.erwin02@gmail.com>
2025-10-03 17:56:38 +0100

    removed image and pesquisa.js

--- src/pages/top20Artists.js
+++ src/pages/top20Artists.js
@@ -24,11 +24,11 @@ export default function Estatisticas() {
         
         <ol className="text-sm">
         {top20.map((item, idx) => (
-          <li key={idx} className="relative bg-linear-to-r from-[#212121] from-50% to-[#121212] to-100% pl-4 p-3 py-4 rounded-[10px] mb-5 first:bg-[url(/img/artist_icon_top_100.jpg)] first:max-w-[440px] first:rounded-t-[0px] first:pt-[85%] first:bg-cover first:text-lg first:text-shadow-[0_0px_4px_rgb(0_0_0)]">
+          <li key={idx} className="relative bg-linear-to-r from-[#212121] from-50% to-[#121212] to-100% pl-4 p-3 py-4 rounded-[10px] mb-5">
             <div className="flex gap-3 space-between relative">
-              <p className="pt-2">#{idx + 1}</p>
-              <span className="pt-2">{item.nome}</span>
-              <span className="absolute pt-[11px] text-[#B3B3B3] text-[12px] right-5">{Math.floor(item.msTotal / 60000)} min ouvidos</span>
+              <p className="py-1">#{idx + 1}</p>
+              <span className="py-1">{item.nome}</span>
+              <span className="absolute py-1 text-[#B3B3B3] text-[12px] right-5">{Math.floor(item.msTotal / 60000)} min ouvidos</span>
             </div>
           </li>
         ))}
