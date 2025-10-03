a4a1b661144fbda50dfe907b678ee41597b024d7
joaolourenco22 <joao_22lourenco@hotmail.com>
2025-10-03 15:42:39 +0100

    removed paddind top from _app and updated artistsProfile

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
 
     <div className="min-h-screen flex items-center justify-center bg-black">
-      <div className="w-full max-w-[428px] py-8 space-y-6">
+      <div className="w-full max-w-[428px] space-y-6">
         <main className="container flex-grow">
           <Component {...pageProps} />
         </main>
