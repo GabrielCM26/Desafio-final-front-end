6c6e4b3ee20d5d9b16dac431a0151f38573dbb2c
Felix <felix.erwin02@gmail.com>
2025-10-03 14:58:31 +0100

    design top100artistas

--- src/pages/top100Artists.js
+++ src/pages/top100Artists.js
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import { useRouter } from 'next/router'
 import { obterTopArtistas } from '../utils/dataProcessing';
 import { PiMicrophoneStageFill } from "react-icons/pi";
+import { MdVerified } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
 
 export default function Top100() {
   const topArtistas = obterTopArtistas(100);
@@ -10,7 +12,6 @@ export default function Top100() {
 
   return (
     <div className="max-w-[440px] mx-auto pt-9 px-5">
-      <div>
         <h1 className="flex gap-4 text-left text-2xl font-bold mb-10">
           <span className="text-[#1DB954]"><PiMicrophoneStageFill /></span>
           Your Top 100 Artists
@@ -23,14 +24,16 @@ export default function Top100() {
             <button className="text-xs">All time</button>
           </div>
         </div>
-      </div>
-      <ol className="text-sm list-decimal list-inside">
+      <ol className="text-sm">
         {topArtistas.map((item, idx) => (
-          <li key={idx} className="bg-linear-to-r from-[#212121] from-50% to-[#121212] to-100% p-3 rounded-[10px] mb-3 first:bg-[url(/img/artist_icon_top_100.jpg)] first:max-w-[440px] first:rounded-t-[0px] first:pt-[70%] first:bg-cover">
+          <li key={idx} className="relative bg-linear-to-r from-[#212121] from-50% to-[#121212] to-100% pl-4 p-3 py-4 rounded-[10px] mb-5 first:bg-[url(/img/artist_icon_top_100.jpg)] first:max-w-[440px] first:rounded-t-[0px] first:pt-[85%] first:bg-cover first:text-lg first:text-shadow-[0_0px_4px_rgb(0_0_0)]">
             <Link href="/artistProfile" className="flex gap-3 space-between relative">
-              <img className="rounded-full" src="../img/artist_icon_top_100_s.png" />
-              <span className="pt-1">{item.artista}</span>
-              <span className="pt-[7px] text-base text-[#B3B3B3] right-[20px] text-[12px] absolute"> {item.plays} plays</span>
+              <p className="pt-2">#{idx + 1}</p>
+              <div className="bg-[#121212] text-[#B3B3B3] rounded-full p-2.5 mt-0.5"><FaUser /></div>
+              <span className="pt-2">{item.artista}</span>
+              <div className="text-[#0D99FF] pt-3
+              "><MdVerified /></div>
+              <span className="absolute pt-[11px] text-[#B3B3B3] text-[12px] right-5">{item.plays} plays</span>
             </Link>
           </li>
         ))}
